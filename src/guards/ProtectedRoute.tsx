a053226d7ffbcd6afbf4ac4e827791d89ee54d8a
adityaputra42 <aditiyaputra42@gmail.com>
2026-09-09 11:28:32 +0700

    fix Protectedroute

--- src/guards/ProtectedRoute.tsx
+++ src/guards/ProtectedRoute.tsx
@@ -1,31 +1,3 @@
-// import { Navigate, Outlet } from 'react-router-dom';
-// import { useAuthStore } from '../hooks/useAuth';
-// import MainLayout from '../components/common/MainLayout';
-// import { useEffect, useState } from 'react';
-
-// const ProtectedRoute = () => {
-//   const { isAuthenticated, accessToken } = useAuthStore();
-//   const [hydrated, setHydrated] = useState(false);
-
-//   useEffect(() => {
-//     setHydrated(true);
-//   }, []);
-
-//   if (!hydrated) {
-//     return null; 
-//   }
-//   if (!isAuthenticated && !accessToken) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return (
-//     <MainLayout>
-//       <Outlet />
-//     </MainLayout>
-//   );
-// };
-
-// export default ProtectedRoute;
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStore } from '../hooks/useAuth';
 import MainLayout from '../components/common/MainLayout';
@@ -34,7 +6,7 @@ const ProtectedRoute = () => {
   const { isAuthenticated } = useAuthStore();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/admin/login" replace />;
   }
 
   return (
